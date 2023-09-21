0e58a90033f4b10c44c8c23806472f616bbbbdee
Luciano Dominguez <lucianodominguez@comunicazione21.com>
2023-09-21 14:06:32 +0200

    Started with recipes and ingredients models

--- database/migrations/2023_09_21_113215_create_recipes_table.php
+++ database/migrations/2023_09_21_113215_create_recipes_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('recipes', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('title');
+            $table->string('bigInteger');
         });
     }
 
